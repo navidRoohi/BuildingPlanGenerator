dedc9e934865c11254b2bfc4f4d683c6bc2f462d
Navid Roohi <navidRoohi@users.noreply.github.com>
2016-07-30 16:53:10 -0700

    Add files via upload

---
+++ PlanGenerator.java
@@ -0,0 +1,138 @@
+
+package PlanGenerator;
+/**
+ *
+ * @author navidroohibroojeni
+ */
+import java.util.*;
+
+public class PlanGenerator {
+   
+    // Plan's dimension
+    private int planDimension;
+    // List of building setBacks
+    private List<Double> setBacks;
+    // Check if there is a wall to other sides ? 
+    private boolean[][] up, right, down,left, stopped;    
+
+     public static void main(String[] args) {
+        //  main size
+        int n = 5;  
+        // plot size here (needs to adjust later)
+        DrawLib.setPLotSize(600,400);
+        PlanGenerator plan = new PlanGenerator(n);
+        // turn on double buffering for graphics
+        DrawLib.enableDoubleBuffering();
+        plan.drawPlan();
+    }
+    
+    // Constructor
+    public PlanGenerator(int n) {
+        this.planDimension = n;
+        DrawLib.setHorizontalSize(0, n+2);
+        DrawLib.setVerticalSize(0, n+2);
+        startInit();
+        produce();
+    }
+
+    // Initialization stage
+    private void startInit() {
+        // initialize border block as already visited
+        stopped = new boolean[planDimension+2][planDimension+2];
+        for (int x = 0; x < planDimension+2; x++) {
+            stopped[x][0] = true;
+            stopped[x][planDimension+1] = true;
+        }
+        for (int y = 0; y < planDimension+2; y++) {
+            stopped[0][y] = true;
+            stopped[planDimension+1][y] = true;
+        }
+
+        // initialize all walls as 
+        up = new boolean[planDimension+2][planDimension+2];
+        right  = new boolean[planDimension+2][planDimension+2];
+        down = new boolean[planDimension+2][planDimension+2];
+        left  = new boolean[planDimension+2][planDimension+2];
+        for (int x = 0; x < planDimension+2; x++) {
+            for (int y = 0; y < planDimension+2; y++) {
+                up[x][y] = true;
+                right[x][y]  = true;
+                down[x][y] = true;
+                left[x][y]  = true;
+            }
+        }
+    }
+
+
+    // generate the plan
+    private void generate(int x, int y) {
+        stopped[x][y] = true;
+
+        // as long as there is not stopped block
+        while (!stopped[x][y+1] || !stopped[x+1][y] || !stopped[x][y-1] || !stopped[x-1][y]) {
+
+            // select random block (This part needs to have some rules instead of being random) 
+            while (true) {
+                double r = RandomGenerator.uniform(4);
+                if (r == 0 && !stopped[x][y+1]) {
+                    up[x][y] = false;
+                    down[x][y+1] = false;
+                    generate(x, y + 1);
+                    break;
+                }
+                else if (r == 1 && !stopped[x+1][y]) {
+                    right[x][y] = false;
+                    left[x+1][y] = false;
+                    generate(x+1, y);
+                    break;
+                }
+                else if (r == 2 && !stopped[x][y-1]) {
+                    down[x][y] = false;
+                    up[x][y-1] = false;
+                    generate(x, y-1);
+                    break;
+                }
+                else if (r == 3 && !stopped[x-1][y]) {
+                    left[x][y] = false;
+                    right[x-1][y] = false;
+                    generate(x-1, y);
+                    break;
+                }
+            }
+        }
+    }
+
+    // produce the plan starting from lower left
+    private void produce() {
+        generate(1, 1);
+          // build more walls to make it more closed space if designer wish to
+//        for (int i = 0; i < 10; i++) {
+//            int x = n/2 + RandomGenerator.uniform(n/2);
+//            int y = n/2 + RandomGenerator.uniform(n/2);
+//            right[x][y] = left[x+1][y] = true;
+//        }
+        // remove some walls to make it more open space
+        for (int i = 0; i < planDimension; i++) {
+            int x = 1 + RandomGenerator.uniform(planDimension-1);
+            int y = 1 + RandomGenerator.uniform(planDimension-1);
+            up[x][y] = down[x][y+1] = false;
+        }
+    }
+
+    // draw the plan
+    public void drawPlan() {
+        // color
+        DrawLib.setPenColor(DrawLib.GRAY);
+        // wall thickness
+        DrawLib.setPenRadius(.02); // 20 cm
+        for (int x = 1; x <= planDimension; x++) {
+            for (int y = 1; y <= planDimension; y++) {
+                if (down[x][y]) DrawLib.wall(x, y, x+1, y);
+                if (up[x][y]) DrawLib.wall(x, y+1, x+1, y+1);
+                if (left[x][y])  DrawLib.wall(x, y, x, y+1);
+                if (right[x][y])  DrawLib.wall(x+1, y, x+1, y+1);
+            }
+        }
+        DrawLib.show();
+    }
+}
